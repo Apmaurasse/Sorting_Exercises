ccca0735b4a1223ca1e33cee6fb772dcf5958adc
Adam Maurasse <apmaurasse@gmail.com>
2024-04-29 12:56:44 -0400

    Updating the file.

---
+++ bubble.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    const n = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                // Swap elements
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return arr;
+}
+
+module.exports = bubbleSort;
